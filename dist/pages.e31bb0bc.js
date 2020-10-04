739e305fd57b4d9b4ed92a923296a5d4afd63a3c
Raeneva <raeneva.dev@yandex.ru>
2020-10-04 17:26:40 +0300

    Added main part

--- dist/pages.e31bb0bc.js
+++ dist/pages.e31bb0bc.js
@@ -195,9 +195,17 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"normalize.css":"../node_modules/normalize.css/normalize.css","_css_loader":"../../../Users/selin/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"normalize.css":"../node_modules/normalize.css/normalize.css","_css_loader":"../../../Users/selin/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/flexboxgrid2/flexboxgrid2.css":[function(require,module,exports) {
+
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+},{"_css_loader":"../../../Users/selin/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 require('./style.scss');
-},{"./style.scss":"style.scss"}],"../../../Users/selin/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+require('flexboxgrid2');
+},{"./style.scss":"style.scss","flexboxgrid2":"../node_modules/flexboxgrid2/flexboxgrid2.css"}],"../../../Users/selin/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -225,7 +233,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65374" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53909" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
